e436082dae509b3f5aae41a427b7bf0193ea7ceb
Nik <ncaryotakis@gmail.com>
2017-07-16 20:37:15 -0700

    forgot import

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 from twilio.twiml.messaging_response import MessagingResponse
 from datetime import datetime, timedelta, timezone
 import schedule
