19afd949d57c451ef98fb9d90627148a3d9e0504
Nik <ncaryotakis@gmail.com>
2017-07-16 21:30:36 -0700

    added key

--- sheets.py
+++ sheets.py
@@ -2,7 +2,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('canned-e0151733c05d.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name('key.json', scope)
 gc = gspread.authorize(credentials)
 spread = gc.open('canned test')
 
