9f47dcec3bb298d303feba399134e75013d79f26
Nik <ncaryotakis@gmail.com>
2017-07-17 20:37:55 -0700

    workers work

--- run.py
+++ run.py
@@ -6,15 +6,15 @@ from redis import Redis
 
 import pyrebase
 import string
-import sheets
+from sheets import delete
+from lib import *
+
 import os
 
 from rq import Queue
 from worker import conn
 
 q = Queue(connection=conn)
-
-
 
 config = {
   "apiKey": "AIzaSyDfwUxnBYr-1yn4MjiTnJ2Jyyby1OQgm4Q",
@@ -26,133 +26,13 @@ config = {
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
-#holds data for a person and the number of shifts theyve signed up for
-class Person(object):
-	def __init__(self, name, number):
-		self.name   = name
-		self.number = number
-		self.shifts = 1
-
-	def addShift(self):
-		self.shifts += 1
-
-#holds all data for an individual shift (name, number, id, time, place etc)
-class Shift(object):
-
-	#gets values from Firebase based on ID given
-	#date is stored as datetime object so time methods are easier
-	def __init__(self, id):
-		self.id       = id
-		self.path     = "shifts/" + id
-		values        = db.child("shifts").child(id).get().val()
-		self.date     = utc_to_local(datetime.strptime(values['date'], '%Y-%m-%dT%H:%M:00.000Z'))
-		self.name     = values['name']
-		self.number   = clean_number(values['number'])
-		self.lockedIn = values['lockedIn']
-		self.sheet    = values['sheet']
-		self.row      = values['row']
-		self.column   = values['column']
-
-	#return a string that looks nice
-	def __str__(self):
-		string = str(self.date_readable())
-		string += ", ID: " + self.id
-		string += ", Locked in? " + self.lockedIn
-		return string
-
-	#formats the date
-	#Mon Jan 1 at 4:05 PM
-	def date_readable(self):
-		return self.date.strftime("%a %b %-d at %-I:%M %p")
-
-#makes a new shift
-def new_shift(id):
-	return Shift(id)
-
 def test():
     print("here we go")
-    #q.enqueue(update_scoreboard())
-
-#firebase automatically stores things in UTC time, so this changes it to local
-def utc_to_local(utc_dt):
-	return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
-
-#queries database for all shifts tomorrow
-#need UTC time for tomorrow and next day to get all shifts within that range in Firebase
-def shifts_tomorrow():
-	shifts = []
-
-	tomorrow = datetime.utcnow() + timedelta(days=1)
-	nextDay  = tomorrow + timedelta(days=1)
-	tomorrow = tomorrow.strftime("%Y-%m-%dT06:00:00.000Z")
-	nextDay  = nextDay.strftime("%Y-%m-%dT06:00:00.000Z")
-
-	for child in db.child("shifts").order_by_child("date").start_at(tomorrow).end_at(nextDay).get().each():
-		shifts.append(new_shift(child.val()['id']))
-
-	print(status(shifts))
-
-#gets rid of weird symbols people enter as part of their number
-def clean_number(number):
-	clean = ""
-	for i in number:
-		if i.isdigit():
-			clean += i
-	if clean[0] == "1":
-		clean = clean[1:]
-	return clean
-
-#queries database for all shifts that match the number given
-#returns list of Shift objects
-def shifts_from_number(number):
-	shifts = []
-	for child in db.child("shifts").order_by_child("number").equal_to(str(number)).get().each():
-		id_ = child.val()['id']
-		shifts.append(new_shift(id_))
-	return shifts
-
-#finds number of shifts associated with each student ID and stores it in Firebase
-def update_scoreboard():
-	people = []
-	names = []
-	then = datetime.now()
-	for child in db.child("shifts").get().each():
-		name = child.val()["name"]
-		number = child.val()["number"]
-		if name in names:
-			people[names.index(name)].addShift()
-		else:
-			people.append(Person(name, number))
-			names.append(name)
-	data = {}
-	for person in people:
-		this = {}
-		this["name"] = person.name
-		this["shifts"] = person.shifts
-		path = "scoreboard/" + str(person.name)
-		data[path] = this
-	print("scoreboard updated. time elapsed: " + str(datetime.now()-then))
-	db.update(data)
+    #q.enqueue(shifts_from_number, 6502797134)
+    #q.enqueue(update_scoreboard)
+    #q.enqueue(delete, 1, 1, 1)
 
 
-#formats list of Shifts by calling their __str__ method
-#adds some info afterwards
-def status(shifts):
-	message = "Current Shifts:"
-	for shift in shifts:
-		message += "\n" + str(shift)
-	message += "\n\nReply confirm/delete followed by the ID to lock in/cancel a shift"
-	message += "\nOr, reply \'shifts\' to see the status of your shifts"
-	message += "\ni.e. \"confirm #a3\""
-	return message
-
-#returns string of stuff user can do
-def options():
-	message = "reply \'confirm #id\' to lock in your shift"
-	message += "\nreply \'delete #id\' to delete your shift"
-	message += "\n reply \'shifts\' to see your shifts"
-	message += "\n reply \'STOP\' to stop these reminders"
-	return message
 
 app = Flask(__name__)
 
--- worker.py
+++ worker.py
@@ -3,13 +3,18 @@ import os
 import redis
 from rq import Worker, Queue, Connection
 
+from sheets import delete
+from lib import *
+
+
+
 listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
