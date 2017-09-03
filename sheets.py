761acd4a11ce3001630a861e3dd8ba94e596ff99
Nik <ncaryotakis@gmail.com>
2017-09-02 20:59:07 -0700

    fixed whitepsace

--- sheets.py
+++ sheets.py
@@ -2,6 +2,7 @@ import gspread
 from datetime import datetime, timedelta
 from oauth2client.service_account import ServiceAccountCredentials
 
+
 SHEET_NAMES = ['Sat Sep 2', 'Sun Sep 3', 'Mon Sep 4', 'Tue Sep 5', 'Wed Sep 6', 'Thu Sep 7', 'Fri Sep 8', 'Sat Sep 9', 'Sun Sep 10', 'Mon Sep 11', 'Tue Sep 12', 'Wed Sep 13', 'Thu Sep 14', 'Fri Sep 15', 'Sat Sep 16', 'Sun Sep 17', 'Mon Sep 18', 'Tue Sep 19', 'Wed Sep 20', 'Thu Sep 21', 'Fri Sep 22', 'Sat Sep 23', 'Sun Sep 24', 'Mon Sep 25', 'Tue Sep 26', 'Wed Sep 27', 'Thu Sep 28', 'Fri Sep 29', 'Sat Sep 30', 'Sun Oct 1']
 
 scope = ['https://spreadsheets.google.com/feeds']
@@ -51,15 +52,16 @@ def get_today_sheet():
 			return i
 	print("Sheet not found")
 
+
+
 def sheet_data(sheetNum):
 	active = get_sheets()[sheetNum]
 	rawData = active.range('A1:M26')
 	cleanData = [[0 for x in range(13)] for y in range(26)]
 
 	for i, cell in enumerate(rawData):
-		print(cell.value)
 		cleanData[i//13][int(i - 13*(i//13))] = cell.value
-
-	print(cleanData)
+		if len(cell.value)!= 0 and not(cell.value.isspace()):
+			print(cell.value)
 	active.update_cell(1,1,"last synced at: {}".format(datetime.now().strftime('%c')))
 	return(cleanData)
