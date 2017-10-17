55769f62c44ebbd664a0972472ff09cb01435ba4
Nik <ncaryotakis@gmail.com>
2017-10-16 20:11:14 -0700

    15 minutes

--- clock.py
+++ clock.py
@@ -10,10 +10,11 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=17)
 def send_sms():
-		q.enqueue(send_sms)
+	q.enqueue(send_sms)
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', minutes=15)
 def update_clean():
+	print("15 min elapsed. updating database")
 	q.enqueue(clean_sheets)
 	q.enqueue(update_shifts)
 	q.enqueue(update_scoreboard)
