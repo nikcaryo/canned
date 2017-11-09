469d67b16db3d7608d63361ea639956566c157aa
nikcaryo <ncaryotakis@gmail.com>
2017-11-08 19:06:03 -0800

    space

--- sms.py
+++ sms.py
@@ -3,5 +3,4 @@ from sheets import get_sheet_data, get_today_sheet
 
 account_sid = "ACee8ef9cbef1643834353fe2711428162"
 auth_token = 'b863f7fa4942fbc15c1bc304c6cf79d4'
-
 client = Client(account_sid, auth_token)
